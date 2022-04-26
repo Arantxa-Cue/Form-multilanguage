0fc8c1bb1a655473d73b4d78dab2b51f8d8d80fb
Arantxa Cuello <arantxa_312@hotmail.com>
2022-04-26 22:22:07 +0200

    form component states added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import "./App.css";
+import Form from "./components/FormComponent/Form";
 
 function App() {
-  return <></>;
+  return <Form />;
 }
 
 export default App;
