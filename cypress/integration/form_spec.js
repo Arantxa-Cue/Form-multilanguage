0e2d7cb9bad8e7b8fbf5c32cd27d4c663bc70a71
Arantxa Cuello <arantxa_312@hotmail.com>
2022-04-27 17:29:46 +0200

    cypress added

--- cypress/integration/form_spec.js
+++ cypress/integration/form_spec.js
@@ -2,10 +2,12 @@ describe("Form event test", () => {
   it("opens the form page and fill in the form", () => {
     cy.visit("http://localhost:3000/");
 
-    cy.get("eventName").type("Automation Test");
-    cy.get("title").type("Barcelona Event");
-    cy.get("subtitle").type("Music Concert");
-    cy.get("description").type("Last concert of the seasson");
-    cy.get("capacity").type("300");
+    cy.get(".input-container-event").type("Automation Test");
+    cy.get(".title-en").type("Barcelona Event");
+    cy.get(".subtitle-en").type("Music Concert");
+    cy.get(".textarea-en").type("Last concert of the season");
+    cy.get(".input-container").type("300");
+
+    cy.get(".form-button").click();
   });
 });
